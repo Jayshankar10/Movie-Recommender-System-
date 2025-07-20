7e15e70736eb1085f96a9fe74dbbd98e81651a46
jayshankar10 <jayjha.ds@gmail.com>
2025-07-20 20:23:47 +0530

    app.py file updated

--- app.py
+++ app.py
@@ -4,6 +4,36 @@ import os
 import pickle
 import requests
 
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]    
+    distances = similarity[index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x: x[1])[1:6]
+
+    recommended_movie = []
+    for i in movies_list:
+        recommended_movie.append(movies.iloc[i[0]].title)
+
+    return recommended_movie
+         
 
 
 st.title("Movie Recommendation System")
+
+movies_dict = pickle.load(open('Notebook/movies.pkl','rb'))
+similarity = pickle.load(open('Notebook/similarity_metrix.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+Selected_movies_name = st.selectbox(  
+                     "Please select the movie name",                      
+                      movies['title'].values
+                     )
+
+if st.button("Recommend Movies"):
+    recommendation = recommend(Selected_movies_name)
+    for i in recommendation:
+        st.write(i)
+
+
+
+st.balloons()
+
+
